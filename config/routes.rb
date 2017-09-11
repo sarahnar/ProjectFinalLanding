b020ea17c1e13f363b23d585479a5fed056c7414
sarah <clergentsarah@gmail.com>
2017-09-11 01:31:02 -0400

    test sgin devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/signin" => "devise/sessions#new" # custom path to login/sign_in
+    get "/signup" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
   
   devise_for :users
   get '/reservation', to: 'pages_resto#reservation'
-  get '/signup',      to: 'pages_resto#signup'
+  
   get '/contact',     to: 'pages_resto#contact'
   get '/about',       to: 'pages_resto#about'
   root 'pages_resto#home'
