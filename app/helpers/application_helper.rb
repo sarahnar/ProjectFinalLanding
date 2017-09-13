73b3ae93a0f863812abaf4bf9fb35440df4d58ac
sarah <clergentsarah@gmail.com>
2017-09-13 18:59:20 -0400

    add carousel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
-    
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <h2>sentence</h2>
+      <ul>messages</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
       # Returns the full title on a per-page basis.
       def full_title(page_title = '')
         base_title = "Search Resto App"
