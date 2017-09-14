092882721a5668d063857a4fb3f067aaefaf8474
sarah <clergentsarah@gmail.com>
2017-09-14 15:37:14 -0400

    test page session

--- app/controllers/sessions/registrations_controller.rb
+++ app/controllers/sessions/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Sessions::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
    def new
-     super
+     
    end
 
   # POST /resource
@@ -36,12 +36,12 @@ class Sessions::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-   protected
+   #protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-   end
+  #def configure_sign_up_params
+  # devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  #end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- app/controllers/sessions/sessions_controller.rb
+++ app/controllers/sessions/sessions_controller.rb
@@ -1,5 +1,4 @@
 class Sessions::SessionsController < Devise::SessionsController
-  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
  
    before_action :configure_sign_in_params, only: [:create]
